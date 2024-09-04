4f49179a82371b13bdd21ea08e31e591faf475b7
zhenglingpeng <lingzhengpeng@Outlook.com>
2024-09-04 09:20:34 +0800

    chore: 删除无用代码

--- src/_mock/index.js
+++
@@ -1,7 +0,0 @@
-import { setupWorker } from 'msw/browser';
-
-import { mswmMockApi } from './handlers/_testmsw';
-import { ListMockApi } from './handlers/list';
-
-// const handlers = [...orgMockApi];
-export const worker = setupWorker(...mswmMockApi, ...ListMockApi);
